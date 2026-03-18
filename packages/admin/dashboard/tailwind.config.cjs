d5f955e1f457c2543ce67d8a90e0fda646fa6462
Vladyslav <vladprytula.github@gmail.com>
2026-03-18 06:41:24 +0100

    fix: replace remaining @medusajs refs in .cjs/.mts config files

--- packages/admin/dashboard/tailwind.config.cjs
+++ packages/admin/dashboard/tailwind.config.cjs
@@ -1,14 +1,14 @@
 const path = require("path")
 
-// get the path of the dependency "@medusajs/ui"
+// get the path of the dependency "@acmekit/ui"
 const medusaUI = path.join(
-  path.dirname(require.resolve("@medusajs/ui")),
+  path.dirname(require.resolve("@acmekit/ui")),
   "**/*.{js,jsx,ts,tsx}"
 )
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require("@medusajs/ui-preset")],
+  presets: [require("@acmekit/ui-preset")],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", medusaUI],
   darkMode: "class",
   theme: {
